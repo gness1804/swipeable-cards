26f5dd991bd89393ea84c3652c4f0319f3bf8bbf
gness1804 <graham.nessler@gmail.com>
2017-01-29 21:53:57 -0700

    Removes parent nodes' children on swipe

--- cards.js
+++ cards.js
@@ -8,9 +8,11 @@ class Cards {
     this.onEnd = this.onEnd.bind(this);
     this.update = this.update.bind(this);
     this.target = null;
+    this.targetBCR = null;
     this.startX = 0;
     this.currentX = 0;
     this.screenX = 0;
+    this.targetX = 0;
     this.draggingCard = false;
 
     this.addEventListeners();
@@ -25,11 +27,17 @@ class Cards {
   }
 
   onStart(e){
+
+    if (this.target) {
+      return;
+    }
+
     if (!e.target.classList.contains('card')) {
       return;
     }
 
     this.target = e.target;
+    this.targetBCR = this.target.getBoundingClientRect();
     this.startX = e.pageX || e.touches[0].pageX;
     this.currentX = this.startX;
     this.draggingCard = true;
@@ -50,6 +58,14 @@ class Cards {
       return;
     }
 
+    this.targetX = 0;
+
+    let screenX = this.currentX - this.startX;
+
+    if (Math.abs(screenX) > this.targetBCR.width * 0.35) {
+      this.targetX = (screenX > 0) ? this.targetBCR.width : -this.targetBCR.width;
+    }
+
     this.draggingCard = false;
 
   }
@@ -65,11 +81,23 @@ class Cards {
     if (this.draggingCard) {
       this.screenX = this.currentX - this.startX;
     } else {
-      this.screenX += (0 - this.screenX) / 10;
+      this.screenX += (this.targetX - this.screenX) / 4;
     }
 
-    this.target.style.transform = `translateX(${this.screenX}px)`;
+    const normalizedDragDistance = (Math.abs(this.screenX) / this.targetBCR.width);
+    const opacity = 1 - Math.pow(normalizedDragDistance, 3);
 
+    this.target.style.transform = `translateX(${this.screenX}px)`;
+    this.target.style.opacity = opacity;
+
+    const isNearlyInvisible = (opacity < 0.01)
+
+    if (!this.draggingCard ) {
+      if (isNearlyInvisible) {
+        this.target.parentNode.removeChild(this.target);
+        this.target = null;
+      }
+    }
   }
 
 }
